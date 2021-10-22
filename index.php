5f401dbb2e4d1fddd7ffb0b1810da9f4a0e53331
allinformatix <fehmi@allinformatix.com>
2021-10-22 08:59:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
-<H1>Version A 1.9  </H1>
+<H1>Version A 2.0  </H1>
 <H3> Hello - Abu Dhabi and Dubai </H1>
 <img src="/a-abtesting.png">
 <div>
-<?php 
+<?php
+ $hostname = getenv('HTTP_HOST');
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ echo "My Pod Name is : "$hostname ;
  ?>
 </div>
 
