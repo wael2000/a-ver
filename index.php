545d812d295b40682b16423d5685fe0606bc612f
wael2000 <waelibrahim2000@hotmail.com>
2022-03-14 15:54:31 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
-<H1>Version A 1.9.1  </H1>
+<H1>Version A 1.9.2  </H1>
 <H3> Hello - Abu Dhabi and Dubai </H1>
 <img src="/a-abtesting.png">
 <div>
